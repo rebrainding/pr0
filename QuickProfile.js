f6982c262e26004d5b060400f9a274d202550396
rebrainding <j.simanovski@gmx.net>
2015-04-28 23:46:01 +0200

    Update QuickProfile.js

--- QuickProfile.js
+++ QuickProfile.js
@@ -13,9 +13,14 @@
 // @downloadURL	https://raw.githubusercontent.com/rebrainding/pr0/master/QuickProfile.js
 // ==/UserScript==
 
+// Global vars
 var cacheName = 'profileCache';
 var previousProfile = null;
 
+// Modify template before rendering
+modifyTemplate();
+
+// Do css stuff
 $(document).ready(function() {
     // Build cache
     createProfileCache();
@@ -24,7 +29,7 @@ $(document).ready(function() {
     var css = '.wide-button { padding-top: 5px; padding-bottom: 5px; padding-left: 20px; padding-right: 20px; margin-top: 5px; margin-bottom: 5px; } \
 .user.profile { display: inline; } \
 .user.profile > .name { cursor: pointer; } \
-.user.profile > .extended { z-index: 9000; display: none; position: absolute; margin-top: 5px; min-width: 200px; padding: 5px; padding-top: 10px; background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgb(42, 46, 49); border-radius: 2px; } \
+.user.profile > .extended { z-index: 9000; display: none; font-size: 12px; position: absolute; margin-top: 5px; min-width: 250px; padding: 5px; padding-top: 10px; background-color: rgba(0, 0, 0, 0.8); /*border: 1px solid rgb(42, 46, 49);*/ } \
 .user.profile > .extended.loading { display: none; }  \
 .user.profile > .extended > .data-row { display: inline; font-family: sans-serif; } \
 .user.profile > .extended > .data-row:after { clear: both; display: block; content: " "; padding-bottom: 3px; } \
@@ -32,38 +37,8 @@ $(document).ready(function() {
 .user.profile > .extended > .data-row > .right { padding-left: 10px; float: right; } \
 .user.profile > .extended > .data-row > .right.fliese { color: #F5F7F6; } \
 .user.profile > .extended > .data-row > .right.rebrain { color: #EE4D2E; } \
-.comments > .comment-box:last-child { margin-bottom: 95px; } \
+.comments > .comment-box:last-child { margin-bottom: 105px; } \
 ';
-    
-    // Build item template hack
-    var templateReplace = /(<a href="#user\/{item.user}" class="user um{item.mark}">{item.user}<\/a>)/;
-    var templateExtension = '<div class="user profile um{item.mark}" data-showable="0" data-visible="0"><span class="name">{item.user}</span>\
-<div class="extended" data-user="{item.user}"><div class="data-row"><span class="left"><a href="#user\/{item.user}" class="confirm-button wide-button">zum Profil</a></span>\
-<span class="right"><a href="#user\/{item.user}/uploads" class="confirm-button wide-button">alle Uploads</a></span></div></div></div>';
-    
-    // Template hack for items
-    var templateOld = p.View.Stream.Item.prototype.template;
-    var templateNew = templateOld.replace(templateReplace, templateExtension);
-    
-    // Set new template and prevent endless bug
-    p.View.Stream.Item = p.View.Stream.Item.extend({
-		template: templateNew,
-		show: function(rowIndex, itemData, defaultHeight, jumpToComment) {
-			this.parent(rowIndex, itemData, defaultHeight, jumpToComment);
-            previousProfile = null;
-    }});
-    
-    // Build comment template hack
-    var templateReplace = /<a href="#user\/{c.name}" class="user um{c.mark}">{c.name}<\/a>/;
-    var templateExtension = '<div class="user profile um{c.mark}" data-showable="0" data-visible="0"><span class="name">{c.name}</span>\
-<div class="extended" data-user="{c.name}"><div class="data-row"><span class="left"><a href="#user\/{c.name}" class="confirm-button wide-button">zum Profil</a></span>\
-<span class="right"><a href="#user\/{c.name}/uploads" class="confirm-button wide-button">alle Uploads</a></span></div></div></div>';
-    
-    // Template hack for comments
-    templateOld = p.View.Stream.Comments.prototype.template;
-    templateNew = templateOld.replace(templateReplace, templateExtension);
-    
-    p.View.Stream.Comments.prototype.template = templateNew;
     
     // Now bind action to item and comment
     $(document).on('click', 'div.user.profile[data-showable][data-visible]', function() { handleUserProfile(this); });
@@ -73,6 +48,39 @@ $(document).ready(function() {
     
 });
 
+function modifyTemplate() {
+    // Modify stream template (item and comment)
+    refreshTemplate(p.View.Stream.Item, 'item.user', 'item.mark');
+    refreshTemplate(p.View.Stream.Comments, 'c.name', 'c.mark');
+    
+    // Modify stelz template
+    refreshTemplate(p.View.FollowList, 'followed.name', 'followed.mark');
+    
+    // Modify inbox template (thread, sender and comment)
+    refreshTemplate(p.View.Inbox, 't.partner.name', 't.partner.mark');
+    refreshTemplate(p.View.Inbox, 'm.senderName', 'm.senderMark');
+    refreshTemplate(p.View.Inbox, 'm.name', 'm.mark');
+}
+
+function refreshTemplate(view, name, mark) {
+    // Build regex and profile
+    var regex = buildProfileRegex(name, mark);
+    var update= buildProfileTemplate(name, mark);
+    
+    // Replace in template
+    view.prototype.template = view.prototype.template.replace(regex, update);
+}
+
+function buildProfileRegex(name, mark) {
+    return (new RegExp('<a href="#user/{' + name + '}" class="user um{' + mark + '}">{' + name + '}</a>', 'g'));
+}
+
+function buildProfileTemplate(name, mark) {
+    return '<div class="user profile um{' + mark + '}" data-showable="0" data-visible="0"><span class="name">{' + name + '}</span>\
+<div class="extended" data-user="{' + name + '}"><div class="data-row"><span class="left"><a href="#user/{' + name + '}" class="confirm-button wide-button">zum Profil</a></span>\
+<span class="right"><a href="#user/{' + name + '}/uploads" class="confirm-button wide-button">alle Uploads</a></span></div></div></div>';
+}
+
 function createProfileCache() {
     $('body').append('<div id="' + cacheName + '" hidden="hidden"></div>');
 }
@@ -81,7 +89,7 @@ function isProfileCached(user) {
     return ($('body > div#' + cacheName + ' > div[data-user="' + user + '"]').length > 0);
 }
 
-function addProfileInCache(user, score, registration, bann, badges) {
+function addProfileInCache(user, rank, score, registration, bann, badges) {
     var content = '<div data-type="cache" data-user="' + user + '">';
     
     // Handle color
@@ -91,7 +99,9 @@ function addProfileInCache(user, score, registration, bann, badges) {
         var color = 'fliese';
     }
     
-    // Score
+    // Rank and Score
+    //rank = '<span class="user um' + rank + '">' + p.User.MARK[rank] + '</span>';
+    content = content + createCacheLine('Status', rank, color);
     content = content + createCacheLine('Benis', score, color);
     
     // Registration
@@ -152,6 +162,10 @@ function loadProfileFromAPI(user) {
             var user = data.user;
             var badges = data.badges;
             
+            // Rank
+            var rank = p.User.MARK[user.mark];
+            //var rank = user.mark;
+            
             // Score and registration
             var score = user.score;
             var since = (new Date (user.registered * 1000)).relativeTime(true, true);
@@ -167,22 +181,55 @@ function loadProfileFromAPI(user) {
                 }
             }
             
+            // Static comment badge
+            for (var i = 0; i < p.View.User.BADGE.COMMENTS.COUNTS.length; i++) {
+                if (data.commentCount > p.View.User.BADGE.COMMENTS.COUNTS[i].count) {
+                    var specialBadge = p.copy(p.View.User.BADGE.COMMENTS.TEMPLATE);
+                    specialBadge.description = specialBadge.description.replace(/%c/g, p.View.User.BADGE.COMMENTS.COUNTS[i].count);
+                    specialBadge.link = '#user/' + data.user.name + '/comments/before/' + (data.comments.length ? data.comments.first().created + 1 : 0);
+                    specialBadge.extra = p.View.User.BADGE.COMMENTS.COUNTS[i].name;
+                    specialBadge.css = 'comments';
+                    
+                    data.badges.push(specialBadge);
+                }
+            }
+            
+            // Static year badge
+            var years = Math.floor((Date.now() - data.user.registered * 1000) / (365 * 24 * 60 * 60 * 1000));
+            if (years > 0) {
+                var specialBadge = p.copy(p.View.User.BADGE.YEARS.TEMPLATE);
+                specialBadge.description = specialBadge.description.replace(/%y/g, 'Jahr'.inflect(years));
+                specialBadge.link = '#user/' + data.user.name;
+                specialBadge.extra = years.toString();
+                specialBadge.css = 'years';
+                
+                data.badges.push(specialBadge);
+            }
+            
             // Badges
             var badge = '';
             for (var i = 0; i < badges.length; i++) {
-                badge = badge + ' <a class="badge" href="' + badges[i].link + '" title="' + badges[i].description + '"><img src="/media/badges/' + badges[i].image + '" class="badge" alt="" /></a>';
+                badge = badge + ' <a class="badge" href="http://' + CONFIG.HOST + '/' + badges[i].link.substr(1) + '" title="' + badges[i].description + '"><img src="/media/badges/' + badges[i].image + '" class="badge" alt="" />' + (badges[i].extra != undefined ? '<span class="badge-extra' + (badges[i].css != undefined ? ' badge-' + badges[i].css : '') + '">' + badges[i].extra + '</span>' : '') + '</a>';
             }
             badge = badge.trim();
             
             // Provide to cache
-            addProfileInCache(user.name, score, since, banned, badge);
+            addProfileInCache(user.name, rank, score, since, banned, badge);
         }
     });
 }
 
-function handleUserProfile(element) {
+function handleUserProfile(element, call) {
     // Get node
     element = $('body').find(element);
+    
+    if (call == undefined) {
+        call = 1;
+    }
+    
+    if (call > 2) {
+        return;
+    }
     
     // Hide item
     if (element.attr('data-visible') == 1) {
@@ -197,7 +244,7 @@ function handleUserProfile(element) {
     } else {
         // Close previous profile
         if (previousProfile != null) {
-            handleUserProfile(previousProfile);
+            handleUserProfile(previousProfile, call + 1);
         }
         
         // Load item
